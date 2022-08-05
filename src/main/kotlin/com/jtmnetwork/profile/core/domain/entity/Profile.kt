2706281eae3197e683833f07723dcd39dbc29747
Joe <joetymatthews@gmail.com>
2022-08-05 21:40:20 +0100

    new: add domain classes for profiles.

---
+++ src/main/kotlin/com/jtmnetwork/profile/core/domain/entity/Profile.kt
@@ -0,0 +1,16 @@
+package com.jtmnetwork.profile.core.domain.entity
+
+import com.jtmnetwork.profile.core.domain.constants.AccountStatus
+import com.jtmnetwork.profile.core.domain.model.Subscription
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document("profiles")
+data class Profile(@Id val id: String, val subs: MutableMap<String, Subscription> = HashMap(),
+                   var status: AccountStatus,
+                   val permissions: MutableList<String> = ArrayList(),
+                   var updated: Long = System.currentTimeMillis(),
+                   val joined: Long = System.currentTimeMillis()) {
+
+    constructor(id: String): this(id = id, status = AccountStatus.ONLINE)
+}
